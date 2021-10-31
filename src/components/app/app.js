a74f141d20ad4691a536e4803dc8735b8a6025ff
Alex <fischer84@mail.ru>
2021-10-31 07:52:26 +0300

    ls99, add form

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -17,15 +17,46 @@ class App extends Component {
                 {name: 'Maxim Erden', salary: 3000, increase: false, id: 2},
                 {name: 'Alex Korn', salary: 5000, increase: true, id: 3}
             ]
-        }
+        };
+        this.maxId = 4;
     }
 
     deleteItem = (id) => {
         this.setState(({data}) => {
+            /*  первый способ
             const index = data.findIndex(elem => elem.id === id);
-            console.log(index);
+
+            const before = data.slice(0, index),
+                  after = data.slice(index + 1),
+                  newArr = [...before, ...after];
+
+            return {
+                data: newArr
+            }
+            */
+            return {  // второй вариант
+                data: data.filter(item => item.id !== id)
+            }
+
         })
     }
+
+    addItem = (name, salary) => {
+        const newItem = {
+            name,
+            salary,
+            increase: false,
+            id: this.maxId++
+        }
+
+        this.setState(({data}) => {
+            // const newArr = [...data, newItem];
+            const newArr = data.concat(newItem);
+            return {
+                data: newArr
+            }
+        })
+    };
 
     render() {    
         return (
@@ -40,7 +71,7 @@ class App extends Component {
                 <EmployeesList 
                     data={this.state.data}
                     onDelete={this.deleteItem}/>
-                <EmployeesAddForm/>
+                <EmployeesAddForm onAdd={this.addItem}/>
     
             </div>
         );
--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -1,5 +1,6 @@
-import './employees-add-form.css';
 import {Component} from 'react';
+
+import './employees-add-form.css';
 
 class EmployeesAddForm extends Component {
     constructor(props) {
@@ -16,6 +17,15 @@ class EmployeesAddForm extends Component {
         })
     }
 
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onAdd(this.state.name, this.state.salary);
+        this.setState({
+            name: '',
+            salary: ''
+        })
+    }
+
     render() {
         const {name, salary} = this.state;
 
@@ -23,7 +33,8 @@ class EmployeesAddForm extends Component {
             <div className="app-add-form">
                 <h3>Добавьте нового сотрудника</h3>
                 <form
-                    className="add-form d-flex">
+                    className="add-form d-flex"
+                    onSubmit={this.onSubmit}>
                     <input type="text"
                         className="form-control new-post-label"
                         placeholder="Как его зовут?" 
